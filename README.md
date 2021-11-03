4d97e9eb5dccc1a68d8da34a73164081433d4f27
gdeblasio-alten <giuseppe.deblasio@alten.it>
2021-11-03 10:24:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kx4jlx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kx4jlx)
---
+++ script.js
@@ -0,0 +1,21 @@
+// jQuery esercizio verifica che le due password immesse siano uguali
+function checkPasswords() {}
+
+// Converti tutti gli elementi di un array in una stringa: esempio // Sex,Drugs,Rock,Roll
+let words = ['Sex', 'Drugs', 'Rock', 'Roll'];
+
+// Ordina gli elementi di un array
+let animals = ['cat', 'dog', 'elephant', 'bee', 'ant'];
+
+// Scrivere un oggetto person che contenga (firstname, lastname, e una funzione fullName che ritorni firstname e lastname)
+let person = {
+  firstname: 'Pippo',
+  lastname: 'Franco',
+};
+
+// Itera l'oggetto di seguito
+let dog = {
+  name: 'Bob',
+  age: 1,
+  race: 'pastore tedesco',
+};
